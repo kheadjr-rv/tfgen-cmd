4d18de1da925734f06f5eb24919a735472790bb9
Kevin Head <khead@redventures.com>
2020-08-20 12:15:18 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kheadjr-rv/tfgen-cmd
+
+go 1.14
+
+require (
+	github.com/hashicorp/hcl/v2 v2.6.0
+	github.com/hashicorp/terraform-config-inspect v0.0.0-20200806211835-c481b8bfa41e
+	github.com/zclconf/go-cty v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+
+	"encoding/json"
+
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/hashicorp/terraform-config-inspect/tfconfig"
+	"github.com/zclconf/go-cty/cty"
+)
+
+func main() {
+
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Println("Expected two args: src and name")
+		os.Exit(1)
+	}
+
+	tf, _ := tfconfig.LoadModule(args[0])
+	name := args[1]
+
+	keys := make([]string, 0)
+	for k := range tf.Variables {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	fmt.Printf("%s", module(name, keys))
+
+	for _, key := range keys {
+		fmt.Printf("%s", variable(key, tf.Variables[key]))
+	}
+
+	for k := range tf.Outputs {
+		s := fmt.Sprintf("# module.%s.%s", name, k)
+		fmt.Println(s)
+	}
+}
+
+func module(name string, keys []string) []byte {
+	root := hclwrite.NewEmptyFile()
+
+	block := root.Body().AppendNewBlock("module", []string{name})
+
+	block.Body().SetAttributeValue("source", cty.StringVal("path_goes_here"))
+	block.Body().SetAttributeValue("version", cty.StringVal("version_goes_here"))
+	block.Body().AppendNewline()
+
+	for _, k := range keys {
+		block.Body().SetAttributeRaw(k, tokens([]byte(fmt.Sprintf("var.%s", k))))
+	}
+
+	root.Body().AppendNewline()
+
+	return root.Bytes()
+}
+
+func variable(name string, v *tfconfig.Variable) []byte {
+	root := hclwrite.NewEmptyFile()
+
+	block := root.Body().AppendNewBlock("variable", []string{name})
+
+	if v.Description != "" {
+		block.Body().SetAttributeValue("description", cty.StringVal(v.Description))
+	}
+
+	if v.Type != "" {
+		block.Body().SetAttributeRaw("type", tokens([]byte(v.Type)))
+	}
+
+	if v.Default != nil {
+		value, _ := json.Marshal(v.Default)
+		block.Body().SetAttributeRaw("default", tokens(value))
+	}
+
+	root.Body().AppendNewline()
+
+	return root.Bytes()
+}
+
+func tokens(b []byte) hclwrite.Tokens {
+	return hclwrite.Tokens{
+		&hclwrite.Token{
+			Type:  hclsyntax.TokenIdent,
+			Bytes: b,
+		},
+	}
+}
